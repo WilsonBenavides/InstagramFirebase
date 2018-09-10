5d8539518e89837b58e0e013103371bc23a5fab3
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-09-09 17:58:57 -0700

    ep 02 UIColor and Anchor Extension

---
+++ InstagramFirebase/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by willix on 9/09/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    //    func someRandomeMethod() {
+    //
+    //    }
+    
+}
