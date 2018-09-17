516cbf7a8c79e843f7f7893a1c14c593dc9b6c7b
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-09-17 08:28:39 -0700

    ep 04 User Profile Picture

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -9,13 +9,39 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -79,20 +105,56 @@ class ViewController: UIViewController {
     }()
     
     @objc func handleSignUp() {
-        guard let email = emailTextField.text, email.characters.count > 0 else { return }
-        guard let username = usernameTextField.text, username.characters.count > 0 else { return }
-        guard let password = passwordTextField.text, password.characters.count > 0 else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
-            
+        guard let email = emailTextField.text, email.count > 0 else {return}
+        guard let username = usernameTextField.text, username.count > 0 else {return}
+        guard let password = passwordTextField.text, password.count > 0 else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error: Error?) in
             if let err = error {
-                print("Failed to create user:", err)
+                print("failed to create user", err)
                 return
             }
             
-            print("Successfully created user:", user?.user.uid ?? "")
+            print("successfully create user", user?.user.uid ?? "")
             
-        }
+            guard let image = self.plusPhotoButton.imageView?.image else {return}
+            
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            let fileName = NSUUID().uuidString
+            Storage.storage().reference().child("profile_images").child(fileName).putData(uploadData, metadata: nil, completion: {(metadata, err) in
+                
+                if let err = err {
+                    print("failed to upload profile image:", err)
+                    return
+                }
+                Storage.storage().reference().child("profile_images").child(fileName).downloadURL(completion: { (url, err) in
+                    if let err = err {
+                        print("Failed to get downloadurl", err)
+                        return
+                    }
+                    guard let profileImageURL = url?.absoluteString else {return}
+                    print("Successfully uploaded profile image", profileImageURL)
+                    
+                    guard let uid = user?.user.uid else {return}
+                    let disctionaryValues = ["username": username, "profileImagerl": profileImageURL]
+                    let values = [uid: disctionaryValues]
+                    
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                        if let err = err {
+                            print("failed to save user info into db", err)
+                            return }
+                        
+                        print("Successfully save user info into db")
+                        
+                    })
+                    
+                })
+            })	
+            
+        })
+        
     }
     
     override func viewDidLoad() {
