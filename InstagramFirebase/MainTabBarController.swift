bf967643a89a5e67527e4a04f6b9a190d75fc468
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-09-17 09:24:18 -0700

    ep 05 Main Tab UI and Fetch User

---
+++ InstagramFirebase/MainTabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  MainTabBarController.swift
+//  InstagramFirebase
+//
+//  Created by willix on 17/09/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+    }
+}
